fcf2509294d9eab297d9c7c2d532f5cad8f580c9
Elias Lintula <Elias.Lintula@metropolia.fi>
2026-08-24 11:40:27 +0300

    tunnin loppu

---
+++ mod03/mod03-teht.py
@@ -0,0 +1,23 @@
+import math
+#tehtävä 1
+#nimi = input ("anna nimesi;")
+#print(f"terve, {nimi}!")
+"""
+#tehtävä 2
+r = float(input("anna säde niin lasken ympyrän pinta-alan: "))
+#r = float (r)
+#print (r)
+
+A = math.pi * r ** 2
+print (f"ympyrän pinta-ala on {A:.2f}")
+"""
+
+#tehtävä 3
+
+A = float(input("anna suorakulmion kanta"))
+B = float(input("anna suorakulmion korkeus"))
+
+piiri = 2 * (A + B)
+#piiri2 = 2 * a + 2 * B
+
+print(f'suorakulmion piiri on: {piiri:.2f} ja pinta-ala {(A * B):3f}')
