f4486430ea06e485c6093d71742c733fc32708ab
Elias Lintula <Elias.Lintula@metropolia.fi>
2026-08-25 13:45:48 +0300

    Update mod03-teht.py

--- mod03/mod03-teht.py
+++ mod03/mod03-teht.py
@@ -6,10 +6,10 @@ import random
 #print(f"terve, {nimi}!")
 
 #tehtävä 2
-r = float(input("anna säde niin lasken ympyrän pinta-alan: "))
+#r = float(input("anna säde niin lasken ympyrän pinta-alan: "))
 #r = float (r)
 #print (r)
-
+"""
 A = math.pi * r ** 2
 print (f"ympyrän pinta-ala on {A:.2f}")
 
@@ -23,11 +23,28 @@ piiri = 2 * (A + B)
 #piiri2 = 2 * a + 2 * B
 
 print(f'suorakulmion piiri on: {piiri:.2f} ja pinta-ala {(A * B):3f}')
+"""
+#tehtävä 5
+
+leiviskä_lkm = float(input("anna leivisköjen määrä "))
+naulat_lkm = float(input("anna naulojen määrä "))
+luodit_lkm = float(input("anna luotien määrä "))
+
+naulat_lkm = leiviskä_lkm * 20 + naulat_lkm
+luodit_lkm = naulat_lkm * 32 + luodit_lkm
+
+print("koko massa luoteina: ", luodit_lkm)
+
+massa_g = luodit_lkm * 13.3
+
+print(f"massa nykymittojen mukaan: {massa_g // 1000:.0f} kiloa ja {massa_g % 1000:.2f} grammaa. ")
+
 
 #tehtävä 6
-
+"""
 luku = random.randint(0,9)
 luku2 = random.randint(,9)
 luku3 = random.randint(0,9)
 print (f"{luku}, {luku2}, {luku3}")
 print (f)
+"""
