23c54332f3ef34866bd41653652f62f71ef088a9
eliaskapa <elias.lintula@metropolia.fi>
2026-09-05 18:17:33 +0300

    tehtäviä 5 tehty

--- t bonus/03teht.py
+++ t bonus/03teht.py
@@ -1,9 +1,75 @@
+import math
+import random
+
+#tehtävä 1
 """
 nimesi = input("kerro nimesi: ")
 
 print(f"terve {nimesi}")
+
+#tehtävä 2
+Säde = float(input("mikä on ympyrän säde?: "))
+
+Pintaala = Säde * Säde * math.pi
+
+print(f"ympyrän pinta-ala on: {Pintaala}")
+
+
+#tehtävä 3
+
+kanta = (int(input("mikä on suora kulmion kanta: ")))
+
+korkeus = (int(input("mikä on suora kulmion korkeus: ")))
+
+pintaala = kanta * korkeus
+
+piiri = (kanta + korkeus) * 2
+
+print(f"pintaala on {pintaala}")
+
+print("piiri on ", piiri)
+
+
+#tehtävä 4
+
+luku = int(input("anna luku 1: "))
+
+luku2 = int(input("anna luku 2: "))
+
+luku3 = int(input("anna luku 3: "))
+
+print(luku)
+print(luku2)
+print(luku3)
+
+summa = luku + luku2 + luku3
+
+ka = (luku + luku2 + luku3) / 3
+
+tulo = luku * luku3 * luku2
+
+print("summa on: ", summa)
+
+print("ka on: ", ka)
+
+print("tulo on: ", tulo)
+
+
+
+leiviskät_m = float(input("anna leiviskät "))
+naulat_m = float(input("anna naulat "))
+luodit_m = float(input("anna luodit "))
+
+naulat_m = leiviskät_m * 20 + naulat_m
+luodit_m = naulat_m * 32 + luodit_m
+
+
+grammat = luodit_m * 13.3
+
+kilot = grammat // 1000
+grammat = grammat % 1000
+
+print(f"paino on {kilot:.0f} kiloa ja {grammat:.1f} grammaa")
+
 """
 
-Säde = input("mikä on ympyrän säde?: ")
-
-pinta ala = Säde * Säde * 3.14
