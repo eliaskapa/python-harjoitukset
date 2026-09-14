2d3a3b5189d6246694fcbe796f28789dd625815a
Elias Lintula <Elias.Lintula@metropolia.fi>
2026-09-14 12:52:52 +0300

    mod09 lisäys ja teht 07

--- mod09/mod09-esim.py
+++ mod09/mod09-esim.py
@@ -85,4 +85,31 @@ Koira.tehty
 inventaario = {"hakku", "kokis"}
 inventaario2 = {"hakku", "kokis"}
 
-class player
+class Player:
+    def __init__(self, name, skill_level, inventory):
+        self.name = name
+        self.skill_level = skill_level
+        self.inventory = inventory
+        self.show_info = self.show_info
+
+    def show_info(self):
+        print(f"Pelaajan nimi: {self.name}")
+        print(f"taso: {self.skill_level}")
+        print(f"inventory: {self.inventory}")
+        for item in self.inventory:
+            print(item)
+    def add_item(self, item):
+        self.inventory.add(item)
+
+        
+                  
+
+player1 = Player("Toni", 10, {"map", "knife"})
+player2 = Player("Joni", 20, {"axe"})
+
+#print(f"Pelaajan 1. nimi on {player1.name} taso on {player1.skill_level} ja inventory on {player1.inventory}")
+
+player1.show_info()
+#player2.show_info()
+player1.add_item("key")
+player1.show_info()
---
+++ t bonus/07-teht2.py
@@ -0,0 +1,7 @@
+import random
+
+def noppapeli():
+    tulos = random.randint(1,6)
+    return tulos
+
+print(noppapeli())
